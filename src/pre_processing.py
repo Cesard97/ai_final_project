79962e66b67172d8b40f90fcfe166a097457bed1
Cesard97 <cd.garrido@uniandes.edu.co>
2019-05-04 18:40:57 -0500

    Added pre processing

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -6,6 +6,7 @@ from imutils import face_utils
 from std_msgs.msg import Float32, Float32MultiArray
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from sklearn import preprocessing
 import numpy as np
 import rospy
 import imutils
@@ -17,11 +18,11 @@ class preProcesing:
     def __init__(self):
         self.image = np.zeros((500, 500, 3), np.uint8)
         self.bridge = CvBridge()
+        self.landmarks = np.zeros((68,2))
 
     def cameraCallback(self,img):
         self.image = self.bridge.imgmsg_to_cv2(img, "bgr8")
-        self.image = imutils.resize(self.image, width=500)
-        #print("The Callback is running")
+        #self.image = imutils.resize(self.image, width=500)
 
     def main(self):
         # initialize dlib's face detector (HOG-based) and then create
@@ -31,8 +32,12 @@ class preProcesing:
         # Node init
         rospy.init_node('preProccesing', anonymous=True)
         # Pub and Subs
-        features = rospy.Publisher('features', Float32MultiArray, queue_size=10)
-        rospy.Subscriber("/naoqi_driver/camera/front/image_raw", Image, self.cameraCallback)
+        pubFeatures = rospy.Publisher('features', Float32MultiArray, queue_size=10)
+        rospy.Subscriber("/naoqi_driver/camera/bottom/image_raw", Image, self.cameraCallback)
+        # Vector
+        featureVector = Float32MultiArray()
+        preProcessLandmarks = np.zeros((136,1))
+        # Rate
         rate = rospy.Rate(10)  # 10hz
 
         while not rospy.is_shutdown():
@@ -46,12 +51,11 @@ class preProcesing:
                 # array
                 shape = predictor(gray, rect)
                 shape = face_utils.shape_to_np(shape)
-
+                self.landmarks = shape
                 # convert dlib's rectangle to a OpenCV-style bounding box
                 # [i.e., (x, y, w, h)], then draw the face bounding box
                 (x, y, w, h) = face_utils.rect_to_bb(rect)
                 cv2.rectangle(self.image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
                 # show the face number
                 cv2.putText(self.image, "Face #{}".format(i + 1), (x - 10, y - 10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
@@ -60,11 +64,27 @@ class preProcesing:
                 # and draw them on the image
                 for (x, y) in shape:
                     cv2.circle(self.image, (x, y), 1, (0, 255, 0), -1)
+
+            # Pre-processing
+            for i in range(0,67):
+                preProcessLandmarks[i] = (self.landmarks[i,0])
+            for i in range(68,136):
+                preProcessLandmarks[i] = (self.landmarks[i-68,1])
+
+            preProcessLandmarks = preprocessing.scale(preProcessLandmarks)
+
+            print(preProcessLandmarks)
+            featureVector.data = preProcessLandmarks
+            pubFeatures.publish(featureVector)
+
+
             # show the output image with the face detections + facial landmarks
             cv2.imshow("Output", self.image)
             key = cv2.waitKey(1) & 0xFF
             if key == ord("q"):
                 break
+
+        rate.sleep()
         cv2.destroyAllWindows()
 
 
