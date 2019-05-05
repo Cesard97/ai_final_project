35e47a6de9220fdf40558727b11b3833dff140e0
Adelaida <a.zuluaga10@uniandes.edu.co>
2019-05-05 15:18:37 -0500

    Neural network modified

--- src/neural_network.py
+++ src/neural_network.py
@@ -4,10 +4,12 @@ import scipy.io as sc
 import glob
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout
-from keras.optimizers import Adam
+from keras.layers import Activation
+from keras.optimizers import Nadam
+from keras.optimizers import SGD, RMSprop
 
-nombresLandMark=[]
-for file in glob.glob("archivosSoporteTarea6/TrainingData/marks/*.mat"):
+nombresLandMark = []
+for file in glob.glob("Tarea6/archivosSoporteTarea6/TrainingData/marks/*.mat"):
     nombresLandMark.append(file)
 
 happy = []
@@ -31,13 +33,14 @@ for element in nombresLandMark:
     imag = np.array([vTem.imag])
            ### Vector de caracteristicas
 
-    vector = np.concatenate((real.T,imag.T), axis=0)
+    vector = np.concatenate((real.T, imag.T), axis=0)
 
 
             ### Divide by name each landmark
 
     name = element.split("/")
-    clas = name[3].split("_")
+    clas = name[4].split("_")
+    print(clas)
 
             #### Clasificacion de los vectores de caracteristicas
 
@@ -57,55 +60,51 @@ for element in nombresLandMark:
 
 #datos de entrada
 ones_angry = np.ones(len(angry))
-ones_angry=np.transpose(ones_angry)
+ones_angry = np.transpose(ones_angry)
 ones_disgust = np.ones(len(disgusted))
-ones_disgust=ones_disgust*2
-ones_disgust=np.transpose(ones_disgust)
+ones_disgust = ones_disgust*2
+ones_disgust = np.transpose(ones_disgust)
 ones_fear = np.ones(len(fear))
-ones_fear=ones_fear*3
-ones_fear=np.transpose(ones_fear)
+ones_fear = ones_fear*3
+ones_fear = np.transpose(ones_fear)
 ones_happy = np.ones(len(happy))
-ones_happy=ones_happy*4
-ones_happy=np.transpose(ones_happy)
+ones_happy = ones_happy*4
+ones_happy = np.transpose(ones_happy)
 ones_neutral = np.ones(len(neutral))
-ones_neutral=ones_neutral*5
-ones_neutral=np.transpose(ones_neutral)
+ones_neutral = ones_neutral*5
+ones_neutral = np.transpose(ones_neutral)
 ones_sad = np.ones(len(sad))
-ones_sad=ones_sad*6
-ones_sad=np.transpose(ones_sad)
-
-#angry = np.asmatrix(angry)
-
-#disgust = np.asmatrix(disgusted)
-
-#fear = np.asmatrix(fear)
-
-#sad = np.asmatrix(sad)
-
-#happy = np.asmatrix(happy)
-
-#neutral = np.asmatrix(neutral)
+ones_sad = ones_sad*6
+ones_sad = np.transpose(ones_sad)
 
 
-
-
-training_data = np.concatenate((angry, disgusted, fear, happy, neutral, sad))
+training_data = np.concatenate((angry, disgusted, fear , happy, neutral, sad))
 
 #resultados
 target_data = np.concatenate((ones_angry, ones_disgust, ones_fear, ones_happy, ones_neutral, ones_sad))
 
 model = Sequential()
 
-model.add(Dense(16, input_dim=134, activation='relu')) #one hiddel layer
+model.add(Dense(20, input_dim=134, activation='relu')) #one hiddel layer
+model.add(Dense(20, input_dim= 134, activation='relu'))
+model.add(Dense(20, input_dim= 134, activation='relu'))
 
-model.add(Dense(6, activation='sigmoid'))
+model.add(Dense(1, activation='linear'))
 
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['binary_accuracy'])
-model.fit(training_data[:, :, 0], target_data, epochs=1000)
+model.summary()
+
+opt = RMSprop (lr=0.001)
+
+model.compile(optimizer=opt, loss='mean_squared_error',metrics=['accuracy'])
+model.fit(training_data[:, :, 0], target_data, batch_size=300, epochs=100000, shuffle=False)
 
 scores = model.evaluate(training_data[:, :, 0], target_data)
 
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+print('Clase a la que pertenece el dato 1')
+print(target_data[0])
+print('Clase predecida por el modelo')
+print(training_data[0, :, 0].shape)
 print(model.predict(training_data[:, :, 0]).round())
 
 
